960d8c3fdbe70cc99a82ba48e21e2fe0ec940b4f
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2022-11-04 11:55:53 +0100

    Define Needful Variables

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# These defined variables are used within the main.tf file using var.aws_region
+
+variable "aws_region" {
+  description = "AWS region for all resources."
+
+  type    = string
+  default = "ap-south-1"
+}
