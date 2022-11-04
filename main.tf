5454d6e4ac38bae233169e6f050e54d1e2df07e2
Riccardo Vecchi <riccardo.vecchi@lastminute.com>
2022-11-04 11:53:11 +0100

    Add # --------------------------------------------- # - Lambda Function # - Lambda CloudWatch Log Group # - Lambda Execution IAM Role # - Lambda Execution IAM Role Policy # ---------------------------------------------

--- main.tf
+++ main.tf
@@ -59,3 +59,51 @@ resource "aws_s3_object" "lambda_sg_demo" {
   etag = filemd5(data.archive_file.lambda_sg_demo.output_path)
 }
   
+# ---------------------------------------------
+# - Lambda Function
+# - Lambda CloudWatch Log Group
+# - Lambda Execution IAM Role
+# - Lambda Execution IAM Role Policy
+# ---------------------------------------------
+
+resource "aws_lambda_function" "sg_demo" {
+  function_name = "SGDemo"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_sg_demo.key
+
+  runtime = "nodejs12.x"
+  handler = "demo.handler"
+
+  source_code_hash = data.archive_file.lambda_sg_demo.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "sg_demo" {
+  name = "/aws/lambda/${aws_lambda_function.sg_demo.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
